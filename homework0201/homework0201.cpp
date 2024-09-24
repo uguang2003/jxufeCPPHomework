119a45b1e47b6fd03d546eb80cbf0e894be37bad
UGMEOW <uguang2003@gmail.com>
2024-09-24 19:14:45 +0800

    add

--- homework0201/homework0201.cpp
+++ homework0201/homework0201.cpp
@@ -1,6 +1,29 @@
 ﻿#include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int arr[] = { -3, -2, -1, 0, 1, 2, 3 };
+    int arr2[7] = {};
+    int sum = 0;
+    //将数组中的元素平方
+    for (int i = 0; i < 7; i++)
+	{
+        arr2[i] = arr[i] * arr[i];
+        sum += arr2[i];
+	}
+    //输出平方后的数组
+    cout << "平方后的数据为:";
+    for (int i = 0; i < 7; i++)
+    {
+	    cout << arr2[i] << " ";
+    }
+    cout << endl << "数组和为:" << sum << endl;
+
+    string str = "Hello C++ Program!";
+    cout << str << endl;
+    cout << "字符的长度为:" << str.size() << endl;
+
+    return 0;
 }
